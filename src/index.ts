d4c65939cd0150c80f889e577480ae29f426deb2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-10-26 16:31:57 +0530

    Hono logger Init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
-const app = new Hono()
+const app = new Hono();
+app.use(logger());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+    return c.text('Hello Hono!');
+});
 
-export default app
+app.get('/ping', (c) => {
+    return c.text('pong');
+});
+
+export default app;
